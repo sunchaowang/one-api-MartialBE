2b9302c32720c61b718ef385d7f25033e657405e
SunnyChao <sunchaowang@outlook.com>
2024-08-14 21:11:34 +0800

    修改分组倍率介绍

--- web/src/views/Home/HomeContent.jsx
+++ web/src/views/Home/HomeContent.jsx
@@ -84,7 +84,7 @@ const renderModalTable = (data, provider) => {
 const Index = () => {
   const changelog = [
     '已接入LINUX DO 授权登录',
-    '优化计价分组倍率, 默认用户组(default)为2.5倍, OpenAI直连用户组(openai_direct)为3.5倍',
+    '优化计价分组倍率, 默认用户组(default)分组倍率为2.5倍, OpenAI直连用户组(openai_direct)分组倍率为5倍，Claude直连用户组(claude_direct)分组倍率为5倍',
     '充值汇率为1.5元1刀'
   ];
 
