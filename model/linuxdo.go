856e939ffddcad25efdca8c4b561f9b4d994841a
SunnyChao <sunchaowang@outlook.com>
2024-09-08 21:47:40 +0800

    修改Linuxdo 的赠金

--- model/linuxdo.go
+++ model/linuxdo.go
@@ -4,18 +4,31 @@ import (
 	"fmt"
 	"one-api/common"
 	"one-api/common/config"
+	"one-api/common/logger"
 )
 
-// 注册成功赠送奖励
+// QuotaForNewLinuxDoUser 为新注册的 LinuxDo 用户赠送奖励
 func (user User) QuotaForNewLinuxDoUser() {
-	// 赠送对应的金额
-	linuxDoLevel := float64(user.LinuxDoLevel)
-	if linuxDoLevel < 2 {
-		linuxDoLevel = 0.5;
-	}else  {
-		linuxDoLevel = float64(user.LinuxDoLevel) - 1;
+	var bonusMultiplier float64
+
+	switch {
+	case user.LinuxDoLevel == 0:
+		bonusMultiplier = 0.25
+	case user.LinuxDoLevel == 1:
+		bonusMultiplier = 0.5
+	case user.LinuxDoLevel == 2:
+		bonusMultiplier = 0.75
+	case user.LinuxDoLevel == 3:
+		bonusMultiplier = 1.5
+	default:
+		bonusMultiplier = float64(user.LinuxDoLevel) - 1
 	}
-	var quota = float64(config.QuotaPerUnit) * linuxDoLevel
-	_ = IncreaseUserQuota(user.Id, int(quota))
-	RecordLog(user.Id, LogTypeSystem, fmt.Sprintf("linux do %v级用户注册, 赠送 %s", user.LinuxDoLevel, common.LogQuota(int(quota))))
+
+	quota := int(float64(config.QuotaPerUnit) * bonusMultiplier)
+	if err := IncreaseUserQuota(user.Id, quota); err != nil {
+		logger.SysError("增加用户配额失败：" + err.Error())
+		return
+	}
+
+	RecordLog(user.Id, LogTypeSystem, fmt.Sprintf("linux do %v级用户注册, 赠送 %s", user.LinuxDoLevel, common.LogQuota(quota)))
 }
